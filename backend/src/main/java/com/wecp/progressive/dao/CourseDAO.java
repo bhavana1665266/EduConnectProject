70c0bd6b7c040df3f804cba080875107adb406a7
bhavana1665266 <117448072+bhavana1665266@users.noreply.github.com>
2025-09-26 12:12:16 +0530

    Update CourseDAO.java

--- backend/src/main/java/com/wecp/progressive/dao/CourseDAO.java
+++ backend/src/main/java/com/wecp/progressive/dao/CourseDAO.java
@@ -5,9 +5,9 @@ import com.wecp.progressive.entity.Course;
 import java.util.List;
 
 public interface CourseDAO {
-    int addCourse(Course course);
-    Course getCourseById(int courseId);
-    void updateCourse(Course course);
-    void deleteCourse(int courseId);
-    List<Course> getAllCourses();
+    int addCourse(Course course) throws SQLException;
+    Course getCourseById(int courseId) throws SQLException;
+    void updateCourse(Course course) throws SQLException;
+    void deleteCourse(int courseId) throws SQLException;
+    List<Course> getAllCourses() throws SQLException;
 }
