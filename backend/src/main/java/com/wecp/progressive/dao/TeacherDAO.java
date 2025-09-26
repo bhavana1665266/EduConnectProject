7cf4cd9a51b2e62f058a1aaceee9fc920c7a3504
bhavana1665266 <117448072+bhavana1665266@users.noreply.github.com>
2025-09-26 12:10:23 +0530

    Update TeacherDAO.java

--- backend/src/main/java/com/wecp/progressive/dao/TeacherDAO.java
+++ backend/src/main/java/com/wecp/progressive/dao/TeacherDAO.java
@@ -5,9 +5,9 @@ import com.wecp.progressive.entity.Teacher;
 import java.util.List;
 
 public interface TeacherDAO {
-    int addTeacher(Teacher teacher);
-    Teacher getTeacherById(int teacherId);
-    void updateTeacher(Teacher teacher);
-    void deleteTeacher(int teacherId) ;
-    List<Teacher> getAllTeachers();
+    int addTeacher(Teacher teacher) throws SQLException;
+    Teacher getTeacherById(int teacherId) throws SQLException;
+    void updateTeacher(Teacher teacher) throws SQLException;
+    void deleteTeacher(int teacherId) throws SQLException;
+    List<Teacher> getAllTeachers() throws SQLException;
 }
