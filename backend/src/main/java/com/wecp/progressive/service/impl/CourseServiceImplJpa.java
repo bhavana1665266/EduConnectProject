25353b142afedcff5e89b940e5f5e65c45a17e50
username <wecp-dummy@gmail.com>
2025-09-27 14:03:06 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/service/impl/CourseServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/CourseServiceImplJpa.java
@@ -1,5 +1,62 @@
 package com.wecp.progressive.service.impl;
 
-public class CourseServiceImplJpa  {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.wecp.progressive.entity.Course;
+import com.wecp.progressive.exception.CourseNotFoundException;
+import com.wecp.progressive.repository.CourseRepository;
+import com.wecp.progressive.service.CourseService;
+
+import java.util.*;
+@Service
+public class CourseServiceImplJpa implements CourseService {
+@Autowired
+private CourseRepository courseRepository;
+
+
+
+public CourseServiceImplJpa(CourseRepository courseRepository) {
+    this.courseRepository = courseRepository;
+}
+public List<Course> getAllCourses() 
+{
+return courseRepository.findAll();
+}
+public Course getCourseById(int courseId) throws Exception
+{
+ Optional<Course> c=courseRepository.findById(courseId);
+ if(c.isPresent()){
+    return c.get();
+ }
+ else{
+    throw new CourseNotFoundException("Not found");
+ }
+}
+public Integer addCourse(Course course)
+{
+    Course c=courseRepository.save(course);
+    return c.getCourseId();
+}
+public void updateCourse(Course course) throws Exception{
+    if(courseRepository.findById(course.getCourseId()).get()!=null)
+    {
+        courseRepository.save(course);
+    }
+    else
+    {
+        throw new Exception("course id"+ course.getCourseId() +"not found");
+    }
+}
+   public void deleteCourse(int courseId) throws Exception
+   {
+    if(courseRepository.findById(courseId).isPresent()){
+     courseRepository.deleteById(courseId);
+    }
+   }
+
+   public List<Course> getAllCourseByTeacherId(int teacherId)
+   {
+    return courseRepository.findAll();
+   }
 }
