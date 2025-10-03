3046ed8ea70f6c7ab2a62f55ca4bbe6de56bda57
bhavana1665266 <117448072+bhavana1665266@users.noreply.github.com>
2025-10-03 22:19:11 +0530

    Update UserLoginServiceImpl.java

--- backend/src/main/java/com/wecp/progressive/service/impl/UserLoginServiceImpl.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/UserLoginServiceImpl.java
@@ -1,23 +1,36 @@
 package com.wecp.progressive.service.impl;
-
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+ 
 import com.wecp.progressive.dto.UserRegistrationDTO;
 import com.wecp.progressive.entity.User;
-
-public class UserLoginServiceImpl {
-
-    User createUser(User user) {
-        return null;
+import com.wecp.progressive.repository.UserRepository;
+ 
+public class UserLoginServiceImpl  {
+ 
+    @Autowired
+    private UserRepository userRepository;
+   public  User createUser(User user) {
+     return userRepository.save(user);
     }
-
-    User getUserByUsername(String username) {
-        return null;
+ 
+   public  User getUserByUsername(String username) {
+    return userRepository.findByUsername(username);
     }
-
-    void registerUser(UserRegistrationDTO userRegistrationDTO) {
-
+ 
+   public void registerUser(UserRegistrationDTO userRegistrationDTO) {
+       
+ 
     }
-
-    User getUserDetails(int userId) {
-        return null;
+ 
+  public   User getUserDetails(int userId) {
+return userRepository.findById(userId).get();
+    }
+    public UserDetails loadUserByUsername(String identifier)
+    {
+      return null;
     }
 }
+ 
+ 
