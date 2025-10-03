cae4018a8cbefe4d94911e60846214fc9372a94c
username <wecp-dummy@gmail.com>
2025-10-03 13:08:24 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/jwt/JwtRequestFilter.java
+++ backend/src/main/java/com/wecp/progressive/jwt/JwtRequestFilter.java
@@ -1,5 +1,9 @@
 package com.wecp.progressive.jwt;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 public class JwtRequestFilter  {
+    // private final UserDetailsService userDetailsService;
+    // private final JwtUtil jwtUtil;
 
 }
